5579ce9f1757cee3a4aee8d295d785fb42500d99
cxgxrc <lautaro.tupac.casagrande@gmail.com>
2023-09-09 23:50:14 -0300

    falta imprimir en pantalla, stats 1

--- tabla.js
+++ tabla.js
@@ -7,11 +7,12 @@ let percentage = []
 let finalPercentage = 0
 let min = 0
 let max = 0
-console.log("🚀 ~ file: tabla.js:5 ~ capacitys:", capacitys)
 
 const tabla = document.querySelector('#tabla')
-const tabla2 = document.querySelector('#tabla2')
-const tabla3 = document.querySelector('#tabla3')
+console.log("🚀 ~ file: tabla.js:5 ~ capacitys:", capacitys)
+
+
+
 
 traerDatos()
 
@@ -22,13 +23,13 @@ async function traerDatos(){
      const jsData = await response.json();
      let data = jsData.events
      let capacitys = data.map( item => item.capacity)
-     console.log("🚀 ~ file: tabla.js:19 ~ traerDatos ~ capacitys:", capacitys)
      let maxCapacity = Math.max(...capacitys)
-     console.log("🚀 ~ file: tabla.js:27 ~ traerDatos ~ maxCapacity:", maxCapacity)
      let assistances = data.map( item => item.assistance)
-     console.log("🚀 ~ file: tabla.js:22 ~ traerDatos ~ assistances:", assistances)
+
      calcPercentages(capacitys, assistances)
-     console.log(percentage)
+     let min = Math.min(...percentage)
+     let max = Math.max(...percentage)
+     generalStats()
     } 
     catch(error) {
      console.log(error);
@@ -38,21 +39,37 @@ async function traerDatos(){
 
 function calcPercentages(arrayCapacity, arrayAssistance) {
     let acc = 0;
-    let cont = 0;
+    // let cont = 0;
     arrayCapacity.forEach(element => {
         if(isNaN(arrayAssistance[acc]) != true ) {
             percentage.push(Math.round(arrayAssistance[acc]/(element/100)))
-            cont += 1
+            // cont += 1
         }
         acc += 1
     });
-    let finalPercentage = Math.round((percentage.reduce((accumulator, currentValue) => accumulator + currentValue, 0)) / cont);
-    let min = Math.min(...percentage)
-    console.log("🚀 ~ file: tabla.js:48 ~ calcPercentages ~ min:", min)
-    let max = Math.max(...percentage)
-    console.log("🚀 ~ file: tabla.js:49 ~ calcPercentages ~ max:", max)
+    // let finalPercentage = Math.round((percentage.reduce((accumulator, currentValue) => accumulator + currentValue, 0)) / cont);
+    
+    
+}
 
 
+function generalStats(){
+    let html = ''
+    html += `<tr>
+    <th colspan="3" class="bg-black text-white fs-3 ">Events Statistics</th>
+  </tr>
+  <tr class="m-3">
+    <td>Highest % of assistance</td>
+    <td>Lowest % of assistance</td>
+    <td>Larger capacity</td>
+  </tr>
+  <tr>
+    <td>${max}</td>
+    <td>${min}</td>
+    <td>${maxCapacity}</td>
+  </tr>`
+
+    tabla.innerHTML = html
 }
 
 
